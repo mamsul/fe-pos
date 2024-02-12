79bb26c1fab951c42a3dbde1b3b9a53c53528c9a
mamsul <imamsulthoni.amd@gmail.com>
2024-02-12 23:22:09 +0700

    feat: code setup (tailwind, prettier, eslint)

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex h-screen items-center justify-center">
+      <h1>Point of Sales Application</h1>
     </div>
   );
 }
